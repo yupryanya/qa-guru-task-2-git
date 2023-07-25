e99d261dc0bd86f75aed1d0fc6b477877ab734e0
Yulia Azovtseva <yulia.pryania@gmail.com>
2023-07-25 17:18:18 +0400

    initial commit

---
+++ src/test/java/qa/guru/GoogleTest.java
@@ -0,0 +1,15 @@
+package qa.guru;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleTest {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
---
+++ src/test/java/qa/guru/YandexTest.java
@@ -0,0 +1,14 @@
+package qa.guru;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTest {
+    @Test
+    void successfulSearchTest() {
+        //to do
+
+    }
+}
